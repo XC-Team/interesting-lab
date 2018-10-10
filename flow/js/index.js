81b0fdb69e52c05142cf7495fd609e0920d72459
zenquan <Jomsoucan@gmail.com>
2018-10-10 15:06:53 +0800

    update index.js

--- flow/js/index.js
+++ flow/js/index.js
@@ -1,16 +1,19 @@
-var oUl = document.getElementById('ul1');
-var aLi = oUl.getElementsByTagName('li');
-var iLen = aLi.length;
-var iPage = 1;
-//用来控制getList的开关
-var b = true;
+var init = {
+    aLi: document.querySelectorAll('#ul1 li'),
+    iLen: function(){
+        return this.aLi.length;
+    },
+    iPage: 1,
+    //用来控制getList的开关
+    b: true
+}
 var portUrl = 'http://jomsou.gearhostpreview.com/flow/get_pics.php';
 //初始化数据
 getList(portUrl);
 
 function getList(url) {
     //用ajax从后端接口加载数据
-    ajax('get', url, 'cpage' + iPage, function (data) {
+    ajax('get', url, 'cpage' + init.iPage, function (data) {
         var data = JSON.parse(data);
         //数据加载完成
         if (!data.length) {
@@ -37,17 +40,17 @@ function getList(url) {
             aLi[_index].appendChild(oDiv);
         }
 
-        b = true;
+        init.b = true;
     })
 }
 window.onscroll = function () {
     var _index = getShort();
-    var oLi = aLi[_index];
+    var oLi = init.aLi[_index];
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     if (getTop(oLi) + oLi.offsetHeight < document.documentElement.clientHeight + scrollTop) {
-        if (b) {
-            b = false;
-            iPage++;
+        if (init.b) {
+            init.b = false;
+            init.iPage++;
             getList(portUrl);
         }
     }
@@ -55,11 +58,11 @@ window.onscroll = function () {
 //获取最短的一列
 function getShort() {
     var index = 0;
-    var ih = aLi[index].offsetHeight;
-    for (var i = 1; i < iLen; i++) {
-        if (aLi[i].offsetHeight < ih) {
+    var ih = init.aLi[index].offsetHeight;
+    for (var i = 1; i < init.iLen; i++) {
+        if (init.aLi[i].offsetHeight < ih) {
             index = i;
-            ih = aLi[i].offsetHeight;
+            ih = init.aLi[i].offsetHeight;
         }
     }
 
